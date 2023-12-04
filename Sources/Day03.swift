24008ce30977f79f097a3612e3a9cd2507ef588c
Gruff Johnston <gjohnston@findmypast.com>
2023-12-04 16:52:27 +0000

    Day 3 part 2

--- Sources/Day03.swift
+++ Sources/Day03.swift
@@ -24,7 +24,7 @@ struct Day03: AdventDay {
             let line = lines[lineIndex]
             for index in 0...line.count - 1 {
                 if let number = Int(line[index]) {
-
+                    
                     var fullNumber = 0
                     if index == 0 {
                         
@@ -40,7 +40,7 @@ struct Day03: AdventDay {
                             fullNumber = Int(String("\(number)\(number2)"))!
                         }
                         let surround = getSurroundingCharactersFor(line: lineIndex, index: index, size: fullNumber.size)
-
+                        
                         if surround.isEmpty == false {
                             total += fullNumber
                         }
@@ -64,6 +64,27 @@ struct Day03: AdventDay {
                         if surround.isEmpty == false {
                             total += fullNumber
                         }
+                    }
+                }
+            }
+        }
+        
+        return total
+    }
+    
+    func part2() -> Any {
+        var total = 0
+        
+        for lineIndex in 0...lines.count - 1 {
+            let line = lines[lineIndex]
+        
+            for index in 0...line.count - 1 {
+            
+                if line[index] == "*" {
+                    let surround = getSurroundingNumbersForGear(line: lineIndex, index: index)
+                
+                    if surround.count == 2 {
+                        total += (surround[0] * surround[1])
                     }
                 }
             }
@@ -122,16 +143,89 @@ struct Day03: AdventDay {
         }
         surround = surround.filter { $0 != "." }
         surround = surround.filter { Int($0) == nil }
-
+        
         return surround
     }
     
+    func getNumberAtLocation(line: Int, index: Int) -> Int {
+        var digits :[Int] = []
+        if let originalNumber = Int(lines[line][index]) {
+            if let n1 = Int(lines[line][index - 1]) {
+                if let n2 = Int(lines[line][index - 2]) {
+                    digits.append(n2)
+                    digits.append(n1)
+                    digits.append(originalNumber)
+                } else {
+                    digits.append(n1)
+                    digits.append(originalNumber)
+                    if let p1 = Int(lines[line][index + 1]) {
+                        digits.append(p1)
+                    }
+                }
+            } else {
+                // nothing to the left, lets try to the right
+                if let p1 = Int(lines[line][index + 1]) {
+                    if let p2 = Int(lines[line][index + 2]) {
+
+                        digits.append(originalNumber)
+                        digits.append(p1)
+                        digits.append(p2)
+
+                    } else {
+
+                        digits.append(originalNumber)
+                        digits.append(p1)
+                    }
+                } else {
+                    // nothing to the right, just one digit
+                    digits.append(originalNumber)
+                }
+            }
+        }
+        
+        var string = ""
+        for digit in digits {
+            string.append(String(digit))
+        }
+        
+        return Int(string) ?? 0
+    }
     
-    func part2() -> Any {
-        var total = 0
+    func getSurroundingNumbersForGear(line: Int, index: Int) -> [Int] {
+        var surround: [Int] = []
         
+        if index > 0 {
+            surround.append(getNumberAtLocation(line: line, index: index - 1))
+        }
+        if lines[line].count > index {
+            surround.append(getNumberAtLocation(line: line, index: index + 1))
+        }
         
-        return total
+        if line > 0 {
+            
+            surround.append(getNumberAtLocation(line: line - 1, index: index))
+            
+            if index > 0 {
+                surround.append(getNumberAtLocation(line: line - 1, index: index - 1))
+            }
+            if lines[line].count > index {
+                surround.append(getNumberAtLocation(line: line - 1, index: index + 1))
+            }
+        }
+        
+        if lines.count > line + 1 {
+            
+            surround.append(getNumberAtLocation(line: line - 1, index: index))
+            
+            if index > 0 {
+                surround.append(getNumberAtLocation(line: line + 1, index: index - 1))
+            }
+            if lines[line].count > index {
+                surround.append(getNumberAtLocation(line: line + 1, index: index + 1))
+            }
+        }
+        
+        return Array(surround.filter { $0 != 0 }.uniqued())
     }
 }
 
